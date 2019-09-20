bfe7508bc31fb918f369588ca240f72700bf206b
Ryan Jones <ryanjones33@gmail.com>
2019-09-20 14:56:56 -0700

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import _ from 'lodash';
+import './styles/style.css';
+import logo from './images/webpack.png';
+import Data from './data/data.xml';
+
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  const myImage = new Image();
+  myImage.src = logo;
+
+  element.appendChild(myImage);
+
+  console.log(Data);
 
   return element;
 }
